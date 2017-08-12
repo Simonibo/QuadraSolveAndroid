062d8c27d8ea00d5b7a179977213d0857edb29c9
BuildTools <unconfigured@null.spigotmc.org>
2017-08-12 20:23:35 +0200

    added contact button

--- app/src/main/java/de/jamesbeans/quadrasolve/AboutActivity.java
+++ app/src/main/java/de/jamesbeans/quadrasolve/AboutActivity.java
@@ -1,9 +1,13 @@
 package de.jamesbeans.quadrasolve;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -17,5 +21,14 @@ public class AboutActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         assert ab != null;
         ab.setDisplayHomeAsUpEnabled(true);
+
+        Button contact = (Button) findViewById(R.id.contact);
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.simonbohnen.me/contact"));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
